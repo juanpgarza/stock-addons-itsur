69629d7475a6ea1fcfd38a48d250e5a93edf3eff
Juan Pablo Garza <juanp@juanpgarza.com>
2021-12-14 21:10:00 -0300

    [12.0][IMP] t764 stock_tier_validation: agrego campo para mostrar y poder filtrar por quien aprobó la validación

--- stock_tier_validation/models/stock_picking.py
+++ stock_tier_validation/models/stock_picking.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Open Source Integrators
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class StockPicking(models.Model):
@@ -10,6 +10,16 @@ class StockPicking(models.Model):
     _state_from = ['assigned', 'waiting', 'confirmed']
     _state_to = ['done']
 
+    review_done_by_users = fields.Char(string='Aprobado Por',compute="_compute_review_user_ids",store=True)
+
+    @api.depends('review_ids')
+    def _compute_review_user_ids(self):
+        for rec in self:
+            if rec.review_ids:
+                rec.review_done_by_users = ', '.join(rec.review_ids.mapped("done_by.name"))
+            else:
+                rec.review_done_by_users = False
+    
     def action_done(self):
         self.write({'state': 'done'})
         res = super().action_done()
